bb8da99b303344aa71c0b4c6451c0de1671d6ff4
Adam Sweeney <adam.sweeney@gmail.com>
2012-03-23 08:58:55 -0400

    test comment

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
-
+	
   def index
+    @mc = MyClass.new("Adam")
     redirect_to dashboard_url if authenticated?
   end
 
---
+++ lib/my_class.rb
@@ -0,0 +1,17 @@
+
+
+	class MyClass < ActiveRecord::Base
+		attr_accessor :name
+
+		def initialize(name="world")
+			@name = name;
+		end
+
+		def SayHello
+			"Hello #{@name}";
+		end
+
+	end
+
+
+
